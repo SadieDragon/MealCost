14165eda7cae49f382419fbe995c18db8df5b013
SadieDragon <53710912+SadieDragon@users.noreply.github.com>
2023-02-21 14:19:38 -0600

    Create add prices.rb

---
+++ add prices.rb
@@ -0,0 +1,16 @@
+
+#ask the user to input [(the store), (the item), (the package size (ie 12 oz)), and (the cost of the item)]
+
+#break that input into an array based on commas (as shown above in the comment)
+#make sure that the stores modified are stored in an array
+
+#read in the JSON of stuffz for each store
+#add to the hash that corresponds with the store tag -> if a new store is detected, abort and ask them to add a new store and acronyms to the program
+
+#if an item is already in the price list, prompt the user with the old price, and the price change
+
+#do you have more to add to that store? 
+#yes -> how many more items? -> repeat the above x times, minus the store tag -> no condition
+#no -> do you have another store to add to? -> (yes -> repeat all of the above steps) (no -> next step)
+
+#update the JSON for the stores that were updated
