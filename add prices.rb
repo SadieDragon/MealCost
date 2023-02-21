70815b3b15f77ae049bbcdc797fbe4c245b70707
SadieDragon <53710912+SadieDragon@users.noreply.github.com>
2023-02-21 14:51:45 -0600

    Update add prices.rb
    Begin work on getting the items in.
    
    kroger, peanut butter, 12 oz, $3.43
    becomes
    ["Kroger", "Peanut Butter", "12 oz", "$3.43"]

--- add prices.rb
+++ add prices.rb
@@ -1,8 +1,29 @@
 
-#ask the user to input [(the store), (the item), (the package size (ie 12 oz)), and (the cost of the item)]
+def full_string_cap(item)
+  item.gsub(/\S+/, &:capitalize)
+end
 
-#break that input into an array based on commas (as shown above in the comment)
+def get_item(message)
+  #ask the user to input [(the store), (the item), (the package size (ie 12 oz)), and (the cost of the item)]
+  puts message
+  @item = gets.chomp()
+
+  #break that input into an array based on commas (as shown above in the comment)
+  @item_array = @item.split(", ")
+  @item_array[0] = full_string_cap(@item_array[0]) #make sure the store is capitalized
+  @item_array[1] = full_string_cap(@item_array[1]) #make sure the item is also capitalized
+  
+  return @item_array
+end
+
+puts "Welcome, user. The format for your input should be as follows, ignoring the parentheses:"
+puts "(store name), (the item you are adding), (package size- ie, 12 oz), (cost of item)"
+puts "And... Please put in the full name. (Food Lion) instead of FL, (Dollar General) instead of DG. Please?"
+
+first_item = get_item("First item, please?")
+
 #make sure that the stores modified are stored in an array
+stores_modified = [first_item[0]]
 
 #read in the JSON of stuffz for each store
 #add to the hash that corresponds with the store tag -> if a new store is detected, abort and ask them to add a new store and acronyms to the program
